ae6dc6d7259eedfe2a407f19f8294645ef7cd548
meron93 <meronyemane18@gmail.com>
2017-09-09 16:39:37 -0700

    test for /app/users/ register user endpoint added

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = 'somerandomstring';
 exports.JWT_EXPIRY = process.env.JWT_SECRET || '7d';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-quotecatcher-app';
--- test/test-server.js
+++ test/test-server.js
@@ -1,19 +1,56 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
 
-const {app} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 
 const should = chai.should();
+const {TEST_DATABASE_URL} = require('../config');
+
+
 chai.use(chaiHttp);
 
-describe('API', function() {
+function tearDownDb() {
+  console.log('Deleting database');
+  return mongoose.connection.dropDatabase();
+}
 
- it('should 200 on GET requests', function() {
-   return chai.request(app)
-     .get('/api/fooooo')
-     .then(function(res) {
-       res.should.have.status(200);
-       res.should.be.json;
-     });
- });
+describe('Quote Catcher API resources', function() {
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+  //beforeEach(function() {
+  //  return 
+  //});
+  afterEach(function() {
+    return tearDownDb();
+  });
+  after(function() {
+    return closeServer();
+  });
+
+  describe('POST endpoint', function() {
+    it('should add a user', function() {
+      const newUser= {
+        username: "Marshawn",
+        firstName: "Marshawn",
+        lastName: "Lynch",
+        password: "password"
+      };
+
+      return chai.request(app)
+        .post('/api/users/')
+        .send(newUser)
+        .then(function(res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.should.be.a('object');
+          res.body.should.include.keys(
+            'username', 'firstName', 'lastName');
+          res.body.username.should.equal(newUser.username);
+          res.body.firstName.should.equal(newUser.firstName);
+          res.body.lastName.should.equal(newUser.lastName);
+        });
+    });
+  });
 });

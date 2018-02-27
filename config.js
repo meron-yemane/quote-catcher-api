2a1c551b0f9a0953e4b9bafc7663005492cf5fc0
meron93 <meronyemane18@gmail.com>
2018-02-27 14:23:58 -0800

    jwt expiry updated in config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/quote-catcher';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'somerandomstring';
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://meron:Nubishis93@ds121674.mlab.com:21674/quote-catcher';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-quotecatcher-app';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ||

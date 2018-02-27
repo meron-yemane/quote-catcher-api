b767ae5b34f2c8094f17ce59ba1b0ef2d0079748
meron93 <meronyemane18@gmail.com>
2018-02-27 12:50:23 -0800

    first push up to alpha_1 branch

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://meron:Nubishis93@ds121674.mlab.com:21674/quote-catcher';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-quotecatcher-app';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ||
                         'https://tutor-mavis-16558.netlify.com';

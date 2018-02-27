0df164e539efcad72cd03a42e5aa47d0fc3263d3
meron93 <meronyemane18@gmail.com>
2018-02-27 14:11:59 -0800

    JWT config corrected

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/quote-catcher';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'somerandomstring';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
 exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://meron:Nubishis93@ds121674.mlab.com:21674/quote-catcher';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-quotecatcher-app';
--- server.js
+++ server.js
@@ -23,8 +23,7 @@ const {quotesRouter} = require('./quotes/router');
 
 app.use(
    cors({
-      origin: CLIENT_ORIGIN
-       // origin: [CLIENT_ORIGIN, 'http://localhost:3000']
+       origin: [CLIENT_ORIGIN, 'http://localhost:3000']
    })
 );
 

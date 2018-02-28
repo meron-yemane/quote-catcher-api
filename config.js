dd901c1c2bb9cb9ef4f6680cc710559a56051480
meron93 <meronyemane18@gmail.com>
2018-02-27 17:43:40 -0800

    config database url altered

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 
-                       global.DATABASE_URL ||
+exports.DATABASE_URL = process.env.MONGODB_URI || 
+                       global.MONGODB_URI ||
                        'mongodb://localhost/quote-catcher';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'somerandomstring';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://meron:Nubishis93@ds121674.mlab.com:21674/quote-catcher';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-quotecatcher-app';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ||
                         'https://tutor-mavis-16558.netlify.com';

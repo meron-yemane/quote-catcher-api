d276c254d5003bb4e9ff31c43683991f25dbfa34
meron93 <meronyemane18@gmail.com>
2018-03-05 15:18:40 -0800

    Client origin altered

--- config.js
+++ config.js
@@ -6,5 +6,4 @@ exports.JWT_SECRET = process.env.JWT_SECRET || 'somerandomstring';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-quotecatcher-app';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ||
-                        'https://mysterious-woodland-75716.herokuapp.com';
-                        // 'https://tutor-mavis-16558.netlify.com';
+                        'https://tutor-mavis-16558.netlify.com';

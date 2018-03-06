5a1d1372b64c1971c325c678975b7d5bb01e1b44
meron93 <meronyemane18@gmail.com>
2018-03-05 20:42:07 -0800

    altered config.js clienturl

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ exports.JWT_SECRET = process.env.JWT_SECRET || 'somerandomstring';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-quotecatcher-app';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ||
-                        'https://thawing-lowlands-94708.herokuapp.com/';
+                        'https://young-eyrie-77920.herokuapp.com';
